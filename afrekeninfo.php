d8a157052c46be395993a5f85beaf74dbf723d0c
Melvin Schoppen <baksteen2000@gmail.com>
2021-11-29 14:00:57 +0100

    update account gegevens + afrekeninfo

--- afrekeninfo.php
+++ afrekeninfo.php
@@ -6,26 +6,22 @@ include __DIR__ . "/header.php";
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <title>account maken</title>
+    <title>Afrekenen informatie</title>
 </head>
 
 <body>
 
 <form>
     <div class="container">
-        <h1> Account maken </h1> <br>
+        <h1> Afrekenen </h1> <br>
         <label>Voornaam : </label>
         <input type="text" placeholder="voornaam" name="voornaam" required>
         <label>Achternaam : </label>
         <input type="text" placeholder="achternaam" name="achternaam" required>
         <label>E-mail : </label>
-        <input type="email" placeholder="E-mail" name="email" required>
-        <label>Gebruikersnaam : </label>
-        <input type="text" placeholder="gebruikersnaam" name="username" required>
-        <label>Wachtwoord : </label>
-        <input type="password" placeholder="wachtwoord" name="password" required> <br><br>
+        <input type="email" placeholder="E-mail" name="email" required> <br><br>
 
-        <h2> Addres gegevens</h2>
+        <h2> Verzend gegevens</h2>
         <label>Provincie : </label>
         <input type="text" placeholder="provincie" name="provincie" required>
         <label>Woonplaats : </label>
@@ -38,11 +34,9 @@ include __DIR__ . "/header.php";
         <input type="text" placeholder="postcode" name="postcode" required> <br><br>
         <label for="voorwaarde">Akkoord met <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">voorwaarden</a></label>
         <input type="checkbox" id="voorwaarde" name="voorwaarde" required> <br><br>
-        <button type="submit">Aanmaken</button> <br>
-        <a href="inloggen.php"> Terug naar inloggen </a> <br>
+        <button type="submit">Betalen</button> <br>
+        <a href="cart.php"> Terug naar winkelwagen </a> <br>
         <a href="index.php"> Terug naar de winkel </a>
-    </div>
-</form>
 
 </body>
 

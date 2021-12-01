fa52ddadca504218fb510654078bd20496d2aa54
Gusuu <guusj.bongers@googlemail.com>
2021-12-01 12:43:46 +0100

    niet werkend maar moet even pushen

--- inloggen.php
+++ inloggen.php
@@ -1,28 +1,26 @@
 <?php
 include __DIR__ . "/header.php";
 ?>
-
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <title>inlog pagina</title>
-</head>
-
-<body>
-
-<form>
-    <div class="container">
-        <h1> inloggen </h1> <br>
-        <label>Gebruikersnaam : </label>
-        <input type="text" placeholder="Geef gebruikersnaam" name="username" required>
-        <label>Wachtwoord : </label>
-        <input type="password" placeholder="Geef wachtwoord" name="password" required>
-        <button type="submit">inloggen</button> <br>
-        <a href="maakaccount.php"> Account maken </a> <br>
-        <a href="index.php"> Terug naar de winkel </a>
-    </div>
-</form>
+<div class="container container-sm">
+    <br><h1>Inloggen</h1>
+    <form method="post">
+        <div class="mb-3">
+            <label for="inputUsername1" class="form-label">Gebruikersnaam</label>
+            <input type="text" class="form-control" id="inputUsername1">
+        </div>
+        <div class="mb-3">
+            <label for="inputPassword1" class="form-label">Wachtwoord</label>
+            <input type="password" class="form-control" id="inputPassword1">
+        </div>
+        <!--<div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>-->
+        <button type="submit" class="btn btn-success">Inloggen</button>
+        <a href="maakaccount.php">Nog geen account? Maak er een!</a><br>
+        <a href="index.php">Terug naar de winkel</a>
+    </form>
+</div>
 
 </body>
 </html>
--- maakaccount.php
+++ maakaccount.php
@@ -1,48 +1,88 @@
 <?php
 include __DIR__ . "/header.php";
+
+if(isset($_POST['']))
 ?>
 
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <title>account maken</title>
-</head>
-
-<body>
-
-<form>
-    <div class="container">
-        <h1> Account maken </h1> <br>
-        <label>Voornaam : </label>
-        <input type="text" placeholder="voornaam" name="voornaam" required>
-        <label>Achternaam : </label>
-        <input type="text" placeholder="achternaam" name="achternaam" required>
-        <label>E-mail : </label>
-        <input type="email" placeholder="E-mail" name="email" required>
-        <label>Gebruikersnaam : </label>
-        <input type="text" placeholder="gebruikersnaam" name="username" required>
-        <label>Wachtwoord : </label>
-        <input type="password" placeholder="wachtwoord" name="password" required> <br><br>
-
-        <h2> Addres gegevens</h2>
-        <label>Provincie : </label>
-        <input type="text" placeholder="provincie" name="provincie" required>
-        <label>Woonplaats : </label>
-        <input type="text" placeholder="woonplaats" name="woonplaats" required>
-        <label>Straatnaam : </label>
-        <input type="text" placeholder="straatnaam" name="straatnaam" required>
-        <label>Huisnummer : </label>
-        <input type="number" placeholder="huisnummer" name="huisnummer" required>
-        <label>Postcode : </label>
-        <input type="text" placeholder="postcode" name="postcode" required> <br><br>
-        <label for="voorwaarde">Akkoord met <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">voorwaarden</a></label>
-        <input type="checkbox" id="voorwaarde" name="voorwaarde" required> <br><br>
-        <button type="submit">Aanmaken</button> <br>
-        <a href="inloggen.php"> Terug naar inloggen </a> <br>
-        <a href="index.php"> Terug naar de winkel </a>
-    </div>
-</form>
+<div class="container container-sm">
+    <h1>Account Maken</h1>
+    <form class="row g-3 needs-validation" novalidate>
+        <div class="col-md-4">
+            <label for="inputFirstname" class="form-label">Voornaam</label>
+            <input type="text" class="form-control" id="inputFirstname" placeholder="Voornaam" required>
+        </div>
+        <div class="col-md-4">
+            <label for="inputLastname" class="form-label">Achternaam</label>
+            <input type="text" class="form-control" id="inputLastname" placeholder="Achternaam" required>
+        </div>
+        <div class="col-md-4">
+            <label for="inputUsername" class="form-label">Gebruikersnaam</label>
+            <div class="input-group has-validation">
+                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                <input type="text" class="form-control" id="inputUsername" aria-describedby="inputGroupPrepend" placeholder="Gebruikersnaam" required>
+                <div class="invalid-feedback">
+                    Kies een unieke gebruikersnaam.
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <label for="inputEmail" class="form-label">Email-adres</label>
+            <input type="email" class="form-control" id="inputEmail" placeholder="Email-adres" required>
+        </div>
+        <div class="col-md-8">
+            <label for="inputPassword" class="form-label">Wachtwoord</label>
+            <input type="password" class="form-control" id="inputPassword" placeholder="Wachtwoord" aria-describedby="helpPassword" required>
+            <div id="helpPassword" class="form-text text-muted">Kies een wachtwoord tussen 6-100 tekens.</div>
+        </div>
+        <div class="col-md-6">
+            <label for="inputStreet" class="form-label">Straat</label>
+            <input type="text" class="form-control" id="inputStreet" placeholder="Straatnaam" required>
+        </div>
+        <div class="col-md-2">
+            <label for="inputHousenumber" class="form-label">Huisnummer</label>
+            <input type="text" class="form-control" id="inputHousenumber" required>
+        </div>
+        <div class="col-md-4">
+            <label for="inputState" class="form-label">Provincie</label>
+            <select class="form-select" id="inputState" required>
+                <option selected disabled value="">Kies een provincie</option>
+                <option>Noord-Holland</option>
+                <option>Zuid-Holland</option>
+                <option>Zeeland</option>
+                <option>Utrecht</option>
+                <option>Groningen</option>
+                <option>Friesland</option>
+                <option>Noord-Brabant</option>
+                <option>Overijssel</option>
+                <option>Gelderland</option>
+                <option>Limburg</option>
+                <option>Flevoland</option>
+            </select>
+        </div>
+        <div class="col-md-4">
+            <label for="inputCity" class="form-label">Woonplaats</label>
+            <input type="email" class="form-control" id="inputEmail" placeholder="Email-adres" required>
+        </div>
+        <div class="col-md-4">
+            <label for="inputEmail" class="form-label">Email-adres</label>
+            <input type="email" class="form-control" id="inputEmail" placeholder="Email-adres" required>
+        </div>
+        <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                <label class="form-check-label" for="invalidCheck">
+                    Agree to terms and conditions
+                </label>
+                <div class="invalid-feedback">
+                    You must agree before submitting.
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <button class="btn btn-primary" type="submit">Submit form</button>
+        </div>
+    </form>
+</div>
 
 </body>
 

59613aa263170a6cc7812476394d9a08e5c4626e
Melvin Schoppen <baksteen2000@gmail.com>
2021-12-02 10:21:19 +0100

    Betaal pagina

---
+++ betalen.php
@@ -0,0 +1,14 @@
+<?php
+include __DIR__ . "/header.php";
+?>
+
+    <div class="container">
+    <img src="Public\Img\ideal.png">
+    <form>
+        <br><button class="btn btn-lg btn-success" type="submit">Betalen</button>
+    </form>
+</div>
+
+<?php
+include __DIR__ . "/footer.php";
+?>

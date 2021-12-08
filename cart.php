41c2ce65c4ac043577b43c90382fe085a8558885
CrazyBoy14 <91765318+CrazyBoy14@users.noreply.github.com>
2021-12-08 15:55:34 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -34,107 +34,112 @@ foreach($cart as $id=>$amount){
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <title>Winkelwagen</title>
-</head>
-<body>
-<br>
-<h1 class="winkelmand-center">Winkelmand</h1>
-<div>
-<?php
-$leeg = false;
-
-if($deleteMelding != 0){
-    print($deleteMelding);
-}else if(count($items) < 1){
-    $leeg = true;
-    print("<div class='container container-sm'><div class='alert alert-danger'>Je hebt nog niks in je winkelmand.</div></div>");
-    }
-
-?>
-
-<?php
-$total = 0;
-foreach($items as $item){
-    $total = $total + $item['SellPrice'] * $amounts[$item['StockItemID']];
-}
-
-?>
-    <div id="ResultsArea" class="container container-sm">
-        <div class="row">
-            <div class="col-8">
+    <!DOCTYPE html>
+    <html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Winkelwagen</title>
+    </head>
+    <body>
+    <br>
+    <h1 class="winkelmand-center">Winkelmand</h1>
+    <div>
         <?php
-        foreach ($items as $item){?>
-                <div id="ProductFrame">
-                    <a class="ListItem" href='view.php?id=<?php print($item['StockItemID']) ?>'>
+        $leeg = false;
+        $teVeelBesteld = false;
+
+        if($deleteMelding != 0){
+            print($deleteMelding);
+        }else if(count($items) < 1){
+            $leeg = true;
+            print("<div class='container container-sm'><div class='alert alert-danger'>Je hebt nog niks in je winkelmand.</div></div>");
+        }
+
+
+        ?>
+
+        <?php
+        $total = 0;
+        foreach($items as $item){
+            $total = $total + $item['SellPrice'] * $amounts[$item['StockItemID']];
+        }
+
+        ?>
+        <div id="ResultsArea" class="container container-sm">
+            <div class="row">
+                <div class="col-8">
                     <?php
-                    if (isset($item['ImagePath11111'])) { ?>
-                        <div class="ImgFrame"
-                             style="background-image: url('<?php print "Public/StockItemIMG/" . $item['ImagePath']; ?>'); background-size: 230px; background-repeat: no-repeat; background-position: center;"></div>
-                    <?php } else if (isset($item['BackupImagePath1111'])) { ?>
-                        <div class="ImgFrame"
-                             style="background-image: url('<?php print "Public/StockGroupIMG/" . $item['BackupImagePath'] ?>'); background-size: cover;"></div>
-                    <?php }else{
-                        ?>
-                        <div class="ImgFrame"
-                             style="background-image: url('<?php print "Public/StockGroupIMG/" . $item['ImagePath'][0]['ImagePath'] ?>'); background-size: cover;"></div>
+                    foreach ($items as $item){?>
+                        <div id="ProductFrame">
+                            <a class="ListItem" href='view.php?id=<?php print($item['StockItemID']) ?>'>
+                                <?php
+                                if (isset($item['ImagePath11111'])) { ?>
+                                    <div class="ImgFrame"
+                                         style="background-image: url('<?php print "Public/StockItemIMG/" . $item['ImagePath']; ?>'); background-size: 230px; background-repeat: no-repeat; background-position: center;"></div>
+                                <?php } else if (isset($item['BackupImagePath1111'])) { ?>
+                                    <div class="ImgFrame"
+                                         style="background-image: url('<?php print "Public/StockGroupIMG/" . $item['BackupImagePath'] ?>'); background-size: cover;"></div>
+                                <?php }else{
+                                    ?>
+                                    <div class="ImgFrame"
+                                         style="background-image: url('<?php print "Public/StockGroupIMG/" . $item['ImagePath'][0]['ImagePath'] ?>'); background-size: cover;"></div>
+                                    <?php
+                                }
+                                ?>
+                            </a>
+                            <h1 class="StockItemID">Artikelnummer: <?php print($item['StockItemID']) ?></h1>
+                            <p class="StockItemName"><?php print($item['StockItemName']) ?></p>
+                            <p class="StockItemComments"><h6>€
+                                <?php
+                                print sprintf(" %0.2f", $item['SellPrice']);
+                                print(" Per stuk (incl. BTW)");
+                                ?></h6>
+                            <a role="button" class="btn btn-sm btn-danger text-light" href="cart.php?action=delete&productid=<?php print($item['StockItemID']); ?>">Verwijder</a>
+                            <h4> <?php if(filter_var($item['QuantityOnHand'], FILTER_SANITIZE_NUMBER_INT) < ($amounts[$item['StockItemID']])){
+                                ?></h4><p><?php $amounts[$item['StockItemID']] = filter_var($item['QuantityOnHand'], FILTER_SANITIZE_NUMBER_INT);print ("Sorry er zijn niet meer dan " . filter_var($item['QuantityOnHand'], FILTER_SANITIZE_NUMBER_INT) . " leverbaar."); $teVeelBesteld = true;?></p> <?php } ?>
+                            <form method="post">
+                                <input type="hidden" name="updateQuantityProductId" value="<?php print($item['StockItemID']) ?>">
+                                <h4 class="ItemQuantity">Aantal: <input type="number" name="updateQuantity" class="form-control form-control-sm" value="<?php print($amounts[$item['StockItemID']]); ?>" min="0"></h4>
+                            </form>
+                            <div id="StockItemFrameRight">
+                                <div class="CenterPriceLeftChild">
+                                    <h1 class="StockItemPriceText"><?php
+                                        if($item['SellPrice'] == -1){
+                                            print("Niet leverbaar");
+                                        }else{
+                                            print sprintf(" %0.2f", $item['SellPrice'] * $amounts[$item['StockItemID']]);
+                                            print("</h1> <h6>Subtotaal (incl. BTW) </h6>");
+                                        }
+                                        ?>
+                                </div>
+                            </div>
+                        </div>
                         <?php
                     }
                     ?>
-                    </a>
-                    <div id="StockItemFrameRight">
-                        <div class="CenterPriceLeftChild">
-                            <h1 class="StockItemPriceText"><?php
-                                if($item['SellPrice'] == -1){
-                                    print("Niet leverbaar");
-                                }else{
-                                    print sprintf(" %0.2f", $item['SellPrice'] * $amounts[$item['StockItemID']]);
-                                    print("</h1> <h6>Subtotaal (incl. BTW) </h6>");
-                                }
-                                ?>
+                </div>
+                <?php if($leeg == false){ ?>
+                    <div class="col-4">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Totaal: <?php
+                                    if(!(count($items) < 1)){
+                                        print(sprintf("€ %.2f", $total));
+                                    }
+                                    ?></h5>
+                                <?php if($leeg) {print("<p class=\"card-text\">Er zit nog niks in je winkelmand.</p>");}?>
+                                <p id="text-test"></p>
+                                <a href="afrekeninfo.php" class="btn btn-primary winkelmand-toevoegen-knop">Afrekenen</a>
+                            </div>
                         </div>
                     </div>
-                    <h1 class="StockItemID">Artikelnummer: <?php print($item['StockItemID']) ?></h1>
-                    <p class="StockItemName"><?php print($item['StockItemName']) ?></p>
-                    <p class="StockItemComments"><h6>€
-                        <?php
-                        print sprintf(" %0.2f", $item['SellPrice']);
-                        print(" Per stuk (incl. BTW)");
-                        ?></h6><br>
-                        <a role="button" class="btn btn-sm btn-danger text-light" href="cart.php?action=delete&productid=<?php print($item['StockItemID']); ?>">Verwijder</a>
-                    </p>
-                    <form method="post">
-                        <input type="hidden" name="updateQuantityProductId" value="<?php print($item['StockItemID']) ?>">
-                    <h4 class="ItemQuantity">Aantal: <input type="number" name="updateQuantity" class="form-control form-control-sm" value="<?php print($amounts[$item['StockItemID']]); ?>" min="1"></h4>
-                    </form>
-                </div>
-        <?php
-        }
-        ?>
-            </div>
-        <div class="col-4">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Totaal: <?php
-                        if(!(count($items) < 1)){
-                            print(sprintf("€ %.2f", $total));
-                        }
-                        ?></h5>
-                    <?php if($leeg) {print("<p class=\"card-text\">Er zit nog niks in je winkelmand.</p>");}?>
-                    <p id="text-test"></p>
-                    <a href="afrekenen.php" class="btn btn-primary winkelmand-toevoegen-knop">Afrekenen</a>
-                </div>
-            </div>
-        </div>
-        </div></div>
-</div>
-<hr>
-<center><p><a href='index.php'>Verder winkelen</a></p></center>
-</body>
-</html>
+                <?php } ?>
+            </div></div>
+    </div>
+    <hr>
+    <center><p><a href='index.php'>Terug naar start</a></p></center>
+    </body>
+    </html>
 <?php
 function berekenVerkoopPrijs($adviesPrijs, $btw) {
     $verkoopPrijs = $btw * $adviesPrijs / 100 + $adviesPrijs;
